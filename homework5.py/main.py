88dea572849af6f339a5a2f6d4457596e15d960b
Proscka <proskinaviktoria861@gmail.com>
2024-06-25 16:36:40 +0300

    Add files via upload

---
+++ homework5.py/main.py
@@ -0,0 +1,10 @@
+immutable_var=1,2,"a","b"
+print(immutable_var)
+immutable_var=(1,2,"a","b")
+print(immutable_var)
+mutable_list=([1,2,"a","b","Modified"])
+print(tuple)
+print([1,2,"a","b","Modified"])
+
+
+
